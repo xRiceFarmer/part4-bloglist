0775ed7229cd2c8488f77a399f308fd97823266d
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-23 08:29:33 +0700

    server runs

---
+++ controllers/blogs.js
@@ -0,0 +1,30 @@
+
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/api/blogs', (request, response, next) => {
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = blogsRouter;
