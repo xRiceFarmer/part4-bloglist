65ecf5ca841831566f537fb3b2ee815ec12c5861
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-06-05 15:51:44 +0700

    populate user when create blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,5 @@
-const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,28 +8,43 @@ const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
-const { constants } = require("node:fs/promises");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
   await Blog.insertMany(helper.initialBlogs);
 });
 
+const loginAndGetToken = async () => {
+  const user = {
+    username: "james",
+    password: "1234",
+  };
+
+  const response = await api.post("/api/login").send(user);
+  return response.body.token;
+};
 test("blogs are returned as json", async () => {
+  const token = await loginAndGetToken();
   await api
     .get("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .expect(200)
     .expect("Content-Type", /application\/json/);
 });
-test("application returns the correct amount of notes", async () => {
-  const response = await api.get("/api/blogs");
+test("application returns the correct amount of blogs", async () => {
+  const token = await loginAndGetToken();
+  const response = await api
+    .get("/api/blogs")
+    .set("Authorization", `Bearer ${token}`);
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 test("unique identifier is id", async () => {
-  const response = await api.get("/api/blogs");
+  const token = await loginAndGetToken();
+  const response = await api
+    .get("/api/blogs")
+    .set("Authorization", `Bearer ${token}`);
   const blogs = response.body;
-
   blogs.forEach((blog) => {
     assert.ok(blog.id, "blog has an id property");
     assert.strictEqual(
@@ -39,14 +54,32 @@ test("unique identifier is id", async () => {
     );
   });
 });
-
-test("a valid blog can be added", async () => {
+test("adding a blog fails if token is not provided", async () => {
+  const token = null
   const newBlog = {
     title: "hello",
     url: "asdasdasd",
   };
   await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
+    .send(newBlog)
+    .expect(401)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await helper.blogsInDb();
+  assert.strictEqual(response.length, helper.initialBlogs.length);
+});
+
+test("a valid blog can be added", async () => {
+  const token = await loginAndGetToken();
+  const newBlog = {
+    title: "hello",
+    url: "asdasdasd",
+  };
+  await api
+    .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
@@ -59,12 +92,14 @@ test("a valid blog can be added", async () => {
 });
 
 test("default like is zero", async () => {
+  const token = await loginAndGetToken();
   const newBlog = {
     title: "hello",
     url: "asdasdasd",
   };
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
@@ -73,50 +108,49 @@ test("default like is zero", async () => {
 });
 
 test("blog without title or url will not be added", async () => {
+  const token = await loginAndGetToken();
+
   const invalidBlog = {
     likes: 10,
   };
-  await api.post("/api/blogs").send(invalidBlog).expect(400);
+  await api.post("/api/blogs").set("Authorization", `Bearer ${token}`)
+  .send(invalidBlog).expect(400);
 
-  const response = await api.get("/api/blogs");
+  const response = await api.get("/api/blogs").set("Authorization", `Bearer ${token}`);
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
-test('a blog can be deleted', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
+test("a blog can be deleted", async () => {
+  const token = await loginAndGetToken();
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
 
-    await api
-        .delete(`/api/blogs/${blogToDelete.id}`)
-        .expect(204)
-    
-    const blogsAtEnd = await helper.blogsInDb()
-    const contents = blogsAtEnd.map(r => r.title)
-    assert(!contents.includes(blogToDelete.title))
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length -1)
-})
+  await api.delete(`/api/blogs/${blogToDelete.id}`).set("Authorization", `Bearer ${token}`)
+  .expect(204)
 
-test('a blog can be updated', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToUpdate = blogsAtStart[0]
+  const blogsAtEnd = await helper.blogsInDb();
+  const contents = blogsAtEnd.map((r) => r.title);
+  assert(!contents.includes(blogToDelete.title));
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+});
 
-    const updatedBlog = {
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 4,          
-    }
+test("a blog can be updated", async () => {
+  const token = await loginAndGetToken();
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToUpdate = blogsAtStart[0];
 
-    await api
-        .put(`/api/blogs/${blogToUpdate.id}`)
-        .send(updatedBlog)
-        .expect(200)
+  const updatedBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 4,
+  };
 
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-    assert.strictEqual(blogsAtEnd[0].likes,4)
+  await api.put(`/api/blogs/${blogToUpdate.id}`).set("Authorization", `Bearer ${token}`).send(updatedBlog).expect(200);
 
-})
-
+  const blogsAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  assert.strictEqual(blogsAtEnd[0].likes, 4);
+});
 
 after(async () => {
   await mongoose.connection.close();
